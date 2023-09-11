ee5c9bbbd861e2d64aba699f9da0125a9225f044
edgaroviedo87 <edgar.oviedo87@gmail.com>
2023-09-11 15:50:34 -0600

    Medical project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Routes
+import { AppRoutingModule } from './app-routing.module';
+
+// Components
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+// Services
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    WhyUsComponent,
+    AboutComponent,
+    ServicesComponent,
+    DepartmentsComponent,
+    DoctorsComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
